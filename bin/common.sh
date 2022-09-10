40ba805a1cd7c4ece931bf45b7e73569a50ef79d
rkumar-ezycerts <rkumar.ezycerts@gmail.com>
2022-09-10 21:39:26 +0530

    Keycloak

---
+++ bin/common.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -x
+
+if [ "x$COMMON_CONF" = "x" ]; then
+  COMMON_CONF="$DIRNAME/common.conf"
+else
+  if [ ! -r "$COMMON_CONF" ]; then
+    echo "Config file not found $COMMON_CONF"
+  fi
+
+fi
+if [ -r "$COMMON_CONF" ]; then
+  . "$COMMON_CONF"
+fi
+
+setModularJdk() {
+  "$JAVA" --add-modules=java.se -version > /dev/null 2>&1 && MODULAR_JDK=true || MODULAR_JDK=false
+}
+
+setDefaultModularJvmOptions() {
+  setModularJdk
+  if [ "$MODULAR_JDK" = "true" ]; then
+    DEFAULT_MODULAR_JVM_OPTIONS=`echo $* | $GREP "\-\-add\-modules"`
+    if [ "x$DEFAULT_MODULAR_JVM_OPTIONS" = "x" ]; then
+      # Set default modular jdk options
+      DEFAULT_MODULAR_JVM_OPTIONS="$DEFAULT_MODULAR_JVM_OPTIONS --add-exports=java.base/sun.nio.ch=ALL-UNNAMED"
+      DEFAULT_MODULAR_JVM_OPTIONS="$DEFAULT_MODULAR_JVM_OPTIONS --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED"
+      DEFAULT_MODULAR_JVM_OPTIONS="$DEFAULT_MODULAR_JVM_OPTIONS --add-exports=jdk.unsupported/sun.reflect=ALL-UNNAMED"
+    else
+      DEFAULT_MODULAR_JVM_OPTIONS=""
+    fi
+  fi
+}
---
+++ themes/keycloak/common/resources/web_modules/@patternfly/react-styles.js
@@ -0,0 +1,2 @@
+import"../common/_commonjsHelpers-b1deedd1.js";export{S as StyleSheet,c as css,d as getClassName,b as getInsertedStyles,g as getModifier,a as isModifier,i as isValidStyleDeclaration,p as pickProperties}from"../common/StyleSheet-b0b30799.js";
+//# sourceMappingURL=react-styles.js.map
